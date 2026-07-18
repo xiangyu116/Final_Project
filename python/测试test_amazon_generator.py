a18940593ac0590861877d2e39e417dce9eadb81
xiangyu116 <xiangyu@tcd.ie>
2026-07-18 18:02:58 +0800

    Delete python directory

--- python/测试test_amazon_generator.py
+++
@@ -1,8 +0,0 @@
-from data_generator import generate_amazon_like_data
-
-df = generate_amazon_like_data(n=1000)
-
-print(df.head())
-print(df.shape)
-
-df.to_csv("generated_amazon_data.csv", index=False)
