ff0ead489469394a2c26fc8e083285b9d894d039
xiangyu116 <xiangyu@tcd.ie>
2026-07-18 18:02:45 +0800

    Delete c++ directory

--- c++/csv_reader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CSV_READER_HPP
-#define CSV_READER_HPP
-
-#include <vector>
-#include <string>
-
-std::vector<std::vector<double>> readCSV(const std::string& filename);
-
-#endif
--- c++/normalize.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef NORMALIZE_HPP
-#define NORMALIZE_HPP
-
-#include <vector>
-
-void normalize(std::vector<std::vector<double>>& data);
-
-#endif
